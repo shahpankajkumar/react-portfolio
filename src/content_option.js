de7c6a151f1e6165615467adb52658574d8a3f48
Pankaj Shah <spankaj769819@gmail.com>
2024-07-19 15:46:29 +0530

    intial commit

---
+++ src/content_option.js
@@ -0,0 +1,169 @@
+const logotext = "Pankaj Shah";
+const meta = {
+  title: "Pankaj Shah",
+  description:
+    "I’m Pankaj Shah Full stack devloper,currently working in Doyenhub Software Solution Pvt Ltd",
+};
+
+const introdata = {
+  title: "I’m Pankaj Shah",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I MERN Stack developer",
+  },
+  description:
+    "I’m Pankaj Shah Full stack devloper,currently working in Doyenhub Software Solution Pvt Ltd",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "about my self",
+  aboutme:
+    "As a MERN stack developer, I specialize in building robust web applications using MongoDB, Express.js, React.js, and Node.js, leveraging their full stack capabilities to create efficient and scalable solutions.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Software Developer",
+    where: "Doyenhub Software Solution Pvt Ltd",
+    date: "jan 2023 - Present",
+  },
+  {
+    jobtitle: "React.js Developer",
+    where: "Pretech Solution",
+    date: "jun 2021 - march 2022",
+  },
+];
+
+const skills = [
+  {
+    name: "React.js",
+    value: 100,
+  },
+  {
+    name: "Node.js",
+    value: 90,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "AWS",
+    value: 60,
+  },
+  {
+    name: "MonogDB",
+    value: 75,
+  },
+  {
+    name: "MySQL",
+    value: 75,
+  },
+  {
+    name: "Material UI",
+    value: 100,
+  },
+  {
+    name: "Redux",
+    value: 100,
+  },
+  {
+    name: "Tailwind",
+    value: 40,
+  },
+  {
+    name: "Bootstrap",
+    value: 100,
+  },
+  {
+    name: "Github",
+    value: 80,
+  },
+  {
+    name: "Antd",
+    value: 100,
+  },
+];
+
+const services = [
+  {
+    title: "Web Development",
+    description:
+      "Showcase your expertise in building dynamic and responsive web applications using the MERN stack (MongoDB, Express.js, React.js, Node.js).",
+  },
+  {
+    title: "API Development and Integration",
+    description:
+      "Emphasize your proficiency in designing and implementing RESTful APIs, integrating third-party services, and ensuring seamless communication between different components of a web application",
+  },
+  {
+    title: "Frontend Development",
+    description:
+      "Illustrate your competence in creating intuitive user interfaces (UI) and engaging user experiences (UX) using React.js and related frontend technologies.",
+  },
+  {
+    title: "Backend Development",
+    description:
+      "Describe your proficiency in building robust backend systems using Node.js and Express.js, handling authentication, authorization, and data manipulation.",
+  },
+  {
+    title: "Responsive Design",
+    description:
+      "Mention your capability to develop websites that are responsive across various devices and screen sizes, ensuring a consistent user experience",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "",
+    description: `
+       I developed the Donation Marketing Cloud (DMC), a comprehensive donation management system designed to meet client-specific needs. This system allows users to create templates and design forms using an integrated editor, enhancing the flexibility and customization of donation campaigns. The frontend is built with React.js, leveraging Material UI, Ant Design, Bootstrap, CSS, and SCSS for a polished and responsive user interface. On the backend, Node.js ensures robust server-side functionality. The application utilizes various AWS services to provide scalable and efficient solutions, including AWS RDS for relational database management, DynamoDB for NoSQL data storage, Lambda for serverless computing, Glue for ETL processes, EC2 for scalable compute capacity, Athena for querying data, and S3 for storage. This combination of technologies and services ensures a reliable, high-performance platform for managing donations effectively.
+        `,
+    link: "https://app.charitable.one",
+  },
+  {
+    img: "",
+    description: `
+       Keys To My Wishes is a robust system designed to streamline the process of managing wishes and their associated contacts. Leveraging a frontend built with React.js, CSS, and SCSS, coupled with a backend powered by Node.js and Express.js, the application ensures seamless interaction and responsiveness. MongoDB serves as the database backbone, facilitating scalable data management. Key features include RESTful API development to support core functionalities and the implementation of secure user authentication via Google and Facebook login integrations, ensuring user data privacy and efficient access management.
+        `,
+    link: "https://www.keystomywishes.com",
+  },
+  {
+    img: "",
+    description:
+      ` I developed a comprehensive Real Estate Property Dealing Application using the MERN stack (React.js, MongoDB, Express.js, Node.js) with Cloudinary integration for image management. This full-stack web application facilitates property buying and selling, catering to sellers, buyers, and admins. Sellers can register, list, and manage property details, while buyers can browse properties and contact sellers. Admins oversee user registrations, manage listings, and ensure smooth interactions. Key features include secure authentication and authorization, dynamic property listings with detailed descriptions, pricing, and location, as well as a messaging system for user communication. The application boasts a responsive design for seamless use across devices. My responsibilities included designing and developing a responsive frontend with React.js, implementing robust backend services with Node.js and Express.js, utilizing MongoDB for efficient data management, and integrating Cloudinary for high-quality image uploads. Additionally, I ensured secure user authentication and authorization mechanisms to protect user data and interactions
+        `,
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "spankaj769819@gmail.com",
+  YOUR_FONE: "+91 7698190564",
+  description:
+    "I am specialize in building robust web applications using MongoDB, Express.js, React.js, and Node.js, leveraging their full stack capabilities to create efficient and scalable solutions.",
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/shahpankajkumar",
+  facebook: "https://www.facebook.com/spankaj769819?mibextid=ZbWKwL",
+  linkedin:
+    "https://www.linkedin.com/in/shah-pankaj-7a729018a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
---
+++ src/pages/portfolio/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { dataportfolio, meta } from "../../content_option";
+
+export const Portfolio = () => {
+  return (
+    <HelmetProvider>
+      <Container className="About-header">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> Portfolio | {meta.title} </title>{" "}
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="12">
+            <h1 className="display-4 mb-4"> Portfolio </h1>{" "}
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <div className="mb-5">
+          {dataportfolio.map((data, i) => {
+            return (
+              <Card className="mb-5" >
+                <Card.Body>
+                  <Card.Text style={{color:'grey'}}>
+                  {data.description}
+                  </Card.Text>
+                  <a href={data.link} style={{color:'blue'}}>view project</a>
+                </Card.Body>
+            </Card>
+            );
+          })}
+        </div>
+      
+      </Container>
+    </HelmetProvider>
+  );
+};
